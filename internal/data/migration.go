fbfccb01b77ecba352852737975618604d1e9f5b
Bayu Firmansyah <bayufirmansyah203@gmail.com>
2026-01-25 13:29:44 +0700

    feat: add automigrate database tables

--- internal/data/migration.go
+++ internal/data/migration.go
@@ -1,13 +1,23 @@
 package data
 
 import (
+	"github.com/bayuf/project-POS-APP-golang-string-team/internal/data/entity"
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) error {
 	return db.AutoMigrate(
-	// Example
-	// &entity.User{},
-	// &entity.Product{},
+		&entity.User{},
+		&entity.MenuCategory{},
+		&entity.RestaurantTable{},
+		&entity.PaymentMethod{},
+
+		&entity.Product{},
+		&entity.Inventory{},
+		&entity.Order{},
+		&entity.OrderItem{},
+		&entity.Reservation{},
+		&entity.OTPRequest{},
+		&entity.Notification{},
 	)
 }
