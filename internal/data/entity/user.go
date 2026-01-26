cfc3a6ff25b701981e52d43ab6c85bcbde322860
Bayu Firmansyah <bayufirmansyah203@gmail.com>
2026-01-26 10:37:55 +0700

    feat: add user seeder for admin and super admin

--- internal/data/entity/user.go
+++ internal/data/entity/user.go
@@ -3,6 +3,7 @@ package entity
 import (
 	"time"
 
+	"github.com/bayuf/project-POS-APP-golang-string-team/pkg/utils"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -20,4 +21,56 @@ type User struct {
 
 	OTPRequests   []OTPRequest
 	Notifications []Notification
+}
+
+func SeedUsers() []User {
+	now := time.Now()
+	password, err := utils.HashString("admin12345")
+	if err != nil {
+		panic(err)
+	}
+	users := []User{
+		{
+			ID:           uuid.New(),
+			Name:         "Super Admin",
+			Email:        "super@admin.com",
+			PasswordHash: password,
+			Role:         "superadmin",
+			IsActive:     true,
+			CreatedAt:    now,
+			UpdatedAt:    &now,
+		},
+		{
+			ID:           uuid.New(),
+			Name:         "Bayu Firmansyah",
+			Email:        "bayu19fr@gmail.com",
+			PasswordHash: password,
+			Role:         "admin",
+			IsActive:     true,
+			CreatedAt:    now,
+			UpdatedAt:    &now,
+		},
+		{
+			ID:           uuid.New(),
+			Name:         "Alif Dwi Rahman",
+			Email:        "alifdwirahman.alf@gmail.com",
+			PasswordHash: password,
+			Role:         "admin",
+			IsActive:     true,
+			CreatedAt:    now,
+			UpdatedAt:    &now,
+		},
+		{
+			ID:           uuid.New(),
+			Name:         "Fathoni Nur Habibi",
+			Email:        "habibifathoni1509@gmail.com",
+			PasswordHash: password,
+			Role:         "admin",
+			IsActive:     true,
+			CreatedAt:    now,
+			UpdatedAt:    &now,
+		},
+	}
+
+	return users
 }
